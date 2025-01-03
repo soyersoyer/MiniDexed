df931bf025f81eda978f1dce747b94ec80846fe4
Gergo Koteles <soyer@irl.hu>
2025-01-03 03:00:00 +0100

    suspend cores while waiting
    It works with WaitForEvent, without disabling IRQs.
    
    Before this the WaitForInterrupt was used, and these findigs were useful:
    
    Before checking the atomic variable, disable IRQs to avoid missing the wake-up event.
    
    https://marc.info/?l=linux-arm-kernel&m=114684008423863&w=2
    https://electronics.stackexchange.com/questions/12601/best-pattern-for-wfi-wait-for-interrupt-on-cortex-arm-microcontrolers
    
    add dsb before wfi
    
    https://github.com/torvalds/linux/commit/8553cb67d2318db327071018fc81084cbabccc46
    https://github.com/torvalds/linux/blob/master/arch/arm/mm/proc-v7.S
    https://github.com/torvalds/linux/blob/master/arch/arm64/kernel/idle.c
    https://github.com/torvalds/linux/commit/9cce7a435f89c9e60f244d44da2cf1cf4ed094ac

--- src/minidexed.cpp
+++ src/minidexed.cpp
@@ -435,7 +435,7 @@ void CMiniDexed::Run (unsigned nCore)
 		{
 			while (m_CoreStatus[nCore] != CoreStatusIdle)
 			{
-				// just wait
+				WaitForEvent ();
 			}
 		}
 
@@ -449,9 +449,11 @@ void CMiniDexed::Run (unsigned nCore)
 		while (1)
 		{
 			m_CoreStatus[nCore] = CoreStatusIdle;		// ready to be kicked
+			SendIPI (1, IPI_USER);
+
 			while (m_CoreStatus[nCore] == CoreStatusIdle)
 			{
-				// just wait
+				WaitForEvent ();
 			}
 
 			// now kicked from core 1
@@ -1248,6 +1250,7 @@ void CMiniDexed::ProcessSound (void)
 		{
 			assert (m_CoreStatus[nCore] == CoreStatusIdle);
 			m_CoreStatus[nCore] = CoreStatusBusy;
+			SendIPI (nCore, IPI_USER);
 		}
 
 		// process the TGs assigned to core 1
@@ -1263,7 +1266,7 @@ void CMiniDexed::ProcessSound (void)
 		{
 			while (m_CoreStatus[nCore] != CoreStatusIdle)
 			{
-				// just wait
+				WaitForEvent ();
 			}
 		}
 
